0f673224edf30ccfe2970dc622773f2641abb269
Wilczynski <martaw@usf.edu>
2019-11-13 12:32:04 -0500

    messing around and fixing different backend connections. Looks like get and post are not working as they need to be

--- scripts.js
+++ scripts.js
@@ -2,39 +2,78 @@ function get(url){
   return new Promise((resolve, reject) => {
     const http = new XMLHttpRequest();
     http.onload = function() {
-        resolve({ status: http.status, data: JSON.parse(http.response)});
+        resolve({ status: http.status, score: JSON.parse(http.response)});
     };
     http.open("GET", url);
     http.send();
   });
 }
 
-function post(url, data){
-  data = JSON.stringify(data);
+function post(url, score){
+  score = JSON.stringify(score);
   return new Promise((resolve, reject) => {
     const http = new XMLHttpRequest();
     http.onload = function() {
-      resolve({ status: http.status, data: JSON.parse(http.response)});
+      resolve({ status: http.status, score: JSON.parse(http.response)});
     };
     http.open("POST", url);
     http.setRequestHeader("Content-Type", "application/json");
-    http.send(data);
+    http.send(score);
   })
 }
 
 function formatUrl(url, user){
-  const apiLink = url.concat(user);
+  const apiLink = url + '?id=' + user;
+  sessionStorage.setItem("apiLink", apiLink);
 
   get(apiLink).then(function(response){
 
   if(response.status == 200){
-  const username = response.data.id;
-  const score = response.data.score;
-  window.location.href = "S:/FizzBuzz/mainpage.html";
+  sessionStorage.setItem("user", response.data.id);
+  sessionStorage.setItem("score", response.data.score);
+  window.location.href = "mainpage.html";
   }
   else{
-      post(apiLink, { score: 0 });
-      window.location.href = "S:/FizzBuzz/mainpage.html";
+      post(apiLink, { score: 0 }).then(function(response){
+        if(response.status == 200 || response.status == 201){
+          window.location.href = "mainpage.html";
+        }
+      });
+
   }
+
+  document.getElementById("displayUsername").value = sessionStorage.getItem("user");
+
   });
+}
+
+function updateUserScore(){
+
+  //get our stored values
+  const apiLink = sessionStorage.getItem("apiLink");
+  var score = sessionStorage.getItem("score");
+
+  //increment score
+  score += 1;
+
+  if(score % 5 == 0 && score % 3 == 0){
+    //output fizzbuzz
+    document.getElementById("displayScore").value = "FizzBuzz";
+  }
+  else if(score % 5 == 0){
+    //output Buzz
+    document.getElementById("displayScore").value = "Buzz";
+  }
+  else if(score % 3 == 0){
+    //output Fizz
+    document.getElementById("displayScore").value = "Fizz";
+  }
+  else{
+    //output just the number
+    document.getElementById("displayScore").value = score;
+  }
+
+  sessionStorage.setItem("score", score);
+  post(apiLink, { id: sessionStorage.getItem("user"), score: score });
+
 }
